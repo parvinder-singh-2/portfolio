cbb0b928b3d00e80780aa28bcbe07d2068699291
parvinder-singh-2 <mr.parvindersinghgandhi@gmail.com>
2024-11-10 17:18:15 +0530

    completed about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import aboutImg from '../assets/about-us.jpg';
+import { ABOUT_TEXT } from '../constants';
 
 
 
@@ -12,7 +13,12 @@ const About = () => {
         <div className="flex flex-wrap">
             <div className="w-full lg:w-1/2 lg:p-8">
                 <div className="flex items-center justify-center">
-                    <img src={aboutImg} alt='About PSGandhi'/>
+                    <img className='rounded-2xl' src={aboutImg} alt='About PSGandhi'/>
+                </div>
+            </div>
+            <div className="w-full lg:w-1/2">
+                <div className="flex justify-center lg:justify-start">
+                    <p className='my-2 max-w-xl py-6'>{ABOUT_TEXT}</p>
                 </div>
             </div>
         </div>
